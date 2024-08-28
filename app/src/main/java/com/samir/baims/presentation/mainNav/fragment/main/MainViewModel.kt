9c533b254ea06aa05dabdc32609b93d740f78fc0
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-08-28 19:54:34 +0300

    (1.0) (feature/05-Adding-DI) -> feat: add new fragments required
    - Added XML layout files for new animations: `fade_in.xml`, `scale_up.xml`, `slide_in_from_bottom.xml`, `slide_in_from_bottom_with_fade.xml`, `slide_in_from_left.xml`, `slide_in_from_right.xml`, `slide_in_from_top.xml`, `slide_out_to_bottom.xml`, `slide_out_to_left.xml`, `slide_out_to_right.xml`, `slide_out_to_top.xml`
    - Updated main activity and fragment layouts: `activity_main.xml`, `fragment_main.xml`, `fragment_weather_list.xml`
    - Integrated new fragments and view models: `MainFragment.kt`, `MainViewModel.kt`, `WeatherListFragment.kt`, `WeatherListViewModel.kt`
    - Added navigation resource file: `main_navigation_resource.xml`
    - Updated `AndroidManifest.xml` with new activity declarations
    - Refactored application class: `MyApp.java`
    - Added support for testing: `test.kt`

---
+++ app/src/main/java/com/samir/baims/presentation/mainNav/fragment/main/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.samir.baims.presentation.mainNav.fragment.main
+
+import androidx.lifecycle.ViewModel
+import com.samir.baims.common.base.BaseViewModel
+
+class MainViewModel : BaseViewModel() {
+
+    override fun stop() {
+
+    }
+
+    override fun start() {
+
+    }
+}
